0bdf4066040ea40d6bd62b042ba3b75227b7326b
alexomaset <alexomaset711@gmail.com>
2019-05-27 10:05:01 +0300

    add initial changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
+from .models import Project,Profile,Rate
+from .forms import ProjectForm,ProfileForm,RateForm
+from django.contrib.auth.models import User
+import datetime as dt
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
